4705c966af0f9604aa0c85a777b6b597b3c8ce92
Alex Kouprin <kpr@nearprotocol.com>
2020-02-25 17:31:56 +0300

    install

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+sudo pip3 install -r requirements.txt
+mkdir ~/.mtmr_kpr
+cp -r pic ~/.mtmr_kpr
+cp -r scpt ~/.mtmr_kpr
+cp -r src ~/.mtmr_kpr
 cp preset.json ~/Library/Application\ Support/MTMR/items.json
